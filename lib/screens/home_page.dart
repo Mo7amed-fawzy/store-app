745cbdfd22bbf3701cf73f2b3e4d34d9f6ecc411
Mo7amed-fawzy <m07amed1st@gmail.com>
2024-10-11 22:10:15 +0200

    finish new trend ui page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:store_app/widgets/custom_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,75 +9,43 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0, // دي يعني الارتفاع والمقصد اختلافها عن الحوليها فاللون
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Colors.grey,
-          ),
-        ),
-        title: const Text(
-          'New Trend',
-        ),
-        centerTitle: true,
-        // actions: const [Icon(Icons.shopping_cart)],
-        actions: [
-          IconButton(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0, // دي يعني الارتفاع والمقصد اختلافها عن الحوليها فاللون
+          leading: IconButton(
             onPressed: () {},
             icon: const Icon(
-              FontAwesomeIcons.cartPlus,
+              Icons.arrow_back,
+              color: Colors.grey,
             ),
           ),
-        ],
-      ),
-      body: Container(
-        height: 130,
-        width: 220,
-        decoration: BoxDecoration(boxShadow: [
-          BoxShadow(
-              blurRadius: 40,
-              color: Colors.grey.withOpacity(.2),
-              spreadRadius: 0, // بيقولك هيتحرك مسافه قد اي او مدى تشويش الظل
-              offset: const Offset(10, 10) // مقدار التحرك x,y
-              )
-        ] // بستخدمه علشان اتحكم فالشادو بتاع الكارد
-            ),
-        child: Card(
-          elevation: 10,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'HandBag LV',
-                  style: TextStyle(color: Colors.grey, fontSize: 16),
-                ),
-                const SizedBox(height: 5),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  // الرو اخد المساحه المتاحه ليه فبتالي الاب بيسنتر حاجته فهعمل كروساكسسالايمنت
-                  children: [
-                    const Text(
-                      '\$255',
-                      // r'$255',
-                      style: TextStyle(fontSize: 13),
-                    ),
-                    IconButton(
-                        onPressed: () {},
-                        icon: const Icon(FontAwesomeIcons.heart)),
-                    // Icon(Icons.favorite)
-                  ],
-                )
-              ],
-            ),
+          title: const Text(
+            'New Trend',
           ),
+          centerTitle: true,
+          // actions: const [Icon(Icons.shopping_cart)],
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.cartPlus,
+              ),
+            ),
+          ],
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.only(right: 16, left: 16, top: 65),
+          child: GridView.builder(
+              clipBehavior: Clip.none,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1.2, // كل متزود الhight يقل
+                crossAxisSpacing: 10, // دا محوي x
+                mainAxisSpacing: 100, // دا بتحكم فمحور y الهو الاساسي
+              ), // بستخدمها فالتحكم فالمنظر
+              itemBuilder: (context, index) {
+                return const CustomCard();
+              }),
+        ));
   }
 }
---
+++ lib/widgets/custom_card.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none, // دي البتخلي الصورة تطلع برا الستاك
+      children: [
+        Container(
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(
+                blurRadius: 40,
+                color: Colors.grey.withOpacity(.2),
+                spreadRadius: 0, // بيقولك هيتحرك مسافه قد اي او مدى تشويش الظل
+                offset: const Offset(10, 10) // مقدار التحرك x,y
+                )
+          ] // بستخدمه علشان اتحكم فالشادو بتاع الكارد
+              ),
+          child: Card(
+            elevation: 10,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 9),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'HandBag LV',
+                    style: TextStyle(color: Colors.grey, fontSize: 16),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // الرو اخد المساحه المتاحه ليه فبتالي الاب بيسنتر حاجته فهعمل كروساكسسالايمنت
+                    children: [
+                      const Text(
+                        '\$255',
+                        // r'$255',
+                        style: TextStyle(fontSize: 13),
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(FontAwesomeIcons.heart)),
+                      // Icon(Icons.favorite)
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          right: 32,
+          top: -65,
+          child: ClipOval(
+            child: Image.network(
+              'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D',
+              height: 100,
+              width: 100, // الارتفاع والعرض متساويين علشان شكل الدايره
+              fit: BoxFit.cover, // علشان تبقي خلفية
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
